fd48b56bff6701447a2773a9c525e0f6d5ead41b
eshaparikh86-commits <eshaparikh86@gmail.com>
2026-07-13 16:35:16 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -5,9 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Bot is running!");
+    res.send("AFK Bot Running");
 });
 
 app.listen(PORT, () => {
-    console.log("Web server started on port", PORT);
+    console.log(`Web server running on ${PORT}`);
 });
